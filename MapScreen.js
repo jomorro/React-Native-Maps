3be7c4e173ccc14a493fd632f00ecbcbcac789cd
jomorro <morrowjoe0@gmail.com>
2019-05-01 17:23:32 -0400

    user current location shows in react-maps

---
+++ MapScreen.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import MapView from "react-native-maps"; 
+
+export default class MapScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userLatitude: 0,
+      userLongitude: 0
+    };
+  }
+
+componentDidMount() {
+  this.locationWatchId = navigator.geolocation.watchPosition((pos) => {
+    this.setState({
+        userLatitude: pos.coords.latitude,
+        userLongitude: pos.coords.longitude
+      });
+    }, 
+      err => console.warn(err),
+    {
+      enableHighAccuracy: true
+    }
+  );
+}
+
+componentWillMount() {
+  navigator.geolocation.clearWatch(this.locationWatchId);
+}
+
+  render() {
+    return (
+    <View style={styles.container}>
+      <MapView
+        showsUserLocation
+        followsUserLocation
+        style={styles.map}
+        region={{
+          latitude: this.state.userLatitude,
+          longitude: this.state.userLongitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121
+        }}
+      />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
+
--- android/app/src/main/java/com/mapsproject/MainApplication.java
+++ android/app/src/main/java/com/mapsproject/MainApplication.java
@@ -1,9 +1,8 @@
 package com.mapsproject;
 
 import android.app.Application;
-
-import com.facebook.react.ReactApplication;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage()
+          new MapsPackage()
       );
     }
 
