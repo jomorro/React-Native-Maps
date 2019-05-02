a9627208ddf46ddfbc4d04983e8f7554e011a3b6
jomorro <morrowjoe0@gmail.com>
2019-05-02 13:55:34 -0400

    google api shows location, and selection stays in text input.

--- components/PlaceInput.js
+++ components/PlaceInput.js
@@ -1,22 +1,59 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, TextInput } from 'react-native'
+import { Text, StyleSheet, View, TextInput, TouchableOpacity } from 'react-native'
 import axios from 'axios';
+import _ from 'lodash';
 
 export default class PlaceInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            predictions:[],
+            destinationInput: ""
+        };
+        this.getPlaces = this.getPlaces.bind(this);
+        this.getPlacesDebounced = _.debounce(this.getPlaces, 1000);
+        this.setDestination =this.setDestination.bind(this);
+    }
 
     async getPlaces(input) {
         const result = await axios.get(
-            "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyA0GCw9Vtqr7arl8J1S-UhlK-tvs0pWjsc&input=Mcdonalds&location=39.850064, -86.053082&radius=2000"
+            `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyA0GCw9Vtqr7arl8J1S-UhlK-tvs0pWjsc&input=${input}&location=39.850064, -86.053082&radius=2000`
         );
+        this.setState({ predictions: result.data.predictions });
         console.log(result.data);
+    }
+    setDestination(main_text) {
+        this.setState({destinationInput: main_text });
     }
 
   render() {
+      const { suggestionStyle, main_textStyle, secondary_textStyle, placeInputStyle } = styles;
+      const predictions = this.state.predictions.map(prediction => {
+        const {id, structured_formatting } = prediction;
+      return (
+          <TouchableOpacity key={id} onPress={() => this.setDestination(structured_formatting.main_text)}>
+            <View style={suggestionStyle}>
+                <Text style= {main_textStyle}>{structured_formatting.main_text}</Text>
+                <Text style={secondary_textStyle}>{structured_formatting.secondary_text}</Text>
+            </View>
+          </TouchableOpacity>
+        );
+      });
     return (
-         <TextInput onChangeText={(input) => this.getPlaces(input)}
-     style={styles.placeInputStyle} 
-     placeholder="where to?" 
-     />
+        <View>
+            <TextInput
+            value={this.state.destinationInput}
+            autoCapitalize="none"
+            autoCorrect={false} 
+            onChangeText={input => {
+                 this.setState({ destinationInput: input });
+                 this.getPlacesDebounced(input);
+            }}
+            style={placeInputStyle} 
+            placeholder="where to?" 
+        />
+        {predictions}
+        </View>
     );
   }
 }
@@ -27,5 +64,17 @@ const styles = StyleSheet.create({
         marginTop: 50,
         padding: 5, 
         backgroundColor: "white"
+    },
+    secondary_textStyle: {
+        color: "#777"
+    },
+    main_textStyle: {
+        color: "#000"
+    },
+    suggestionStyle: {
+        borderTopWidth: 0.5,
+        borderColor: "#777",
+        backgroundColor: "white",
+        padding: 15
     }
 });
